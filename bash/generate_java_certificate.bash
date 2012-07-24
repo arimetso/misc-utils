3001b1ce72bd0dd323e1cf589da31b0a1f953ecd
arimetso <github@metson.org>
2012-07-24 21:22:51 +0300

    Throwaway certificates for Java web development.

---
+++ bash/generate_java_certificate.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Generate simple self-signed SSL certificate using JDK keytool.
+
+keystore_file=keystore.dat
+
+test -f "$keystore_file" && echo "`basename $0`: $keystore_file already exists." && exit 1
+keytool -keystore "$keystore_file" -storepass "pas5W0rd" -alias "devkey" -genkey -keyalg "RSA" -validity 3650 -keypass "pas5W0rd"
+
